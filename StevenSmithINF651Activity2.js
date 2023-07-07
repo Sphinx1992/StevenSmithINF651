7587d8e10f437f5891b8fba50d2ba96372050beb
Sphinx1992 <136394744+Sphinx1992@users.noreply.github.com>
2023-07-07 15:13:03 -0500

    Add files via upload
    initial commit

---
+++ StevenSmithINF651Activity2.js
@@ -0,0 +1,56 @@
+document.write("INF 651 Steven Smith Activity 2");
+
+document.write("<br><br><br><br>");
+
+document.write("Question 1:<br> const=myNumber<br><br>");
+ 
+const myNumber = 5;
+typeof Number; 
+
+document.write(`Question 2:<br> typeof Number<br> myNumber = ${myNumber}<br><br>`);
+
+const myOtherNumber = 5;
+typeof Number;
+
+document.write(`Question 3:<br> myOtherNumber = ${myOtherNumber}<br> typeof Number<br><br>`);
+
+let myTotal = myNumber*myOtherNumber;
+typeof Number;
+
+document.write(`Question 4:<br> myTotal = myNumber*myOtherNumber = ${myNumber*myOtherNumber}<br> typeof Number<br><br>`);
+
+var myName;
+typeof String;
+
+let myCombo = myNumber + myOtherNumber;
+typeof String;
+
+document.write(`Question 5:<br> var myName, typeof String<br> let myCombo = myNumber + myOtherNumber = ${myNumber + myOtherNumber}<br> typeof String<br><br>`);
+
+let myOtherCombo = myNumber + myOtherNumber;
+typeof Number;
+
+document.write(`Question 6:<br> typeof Number<br> let myOtherCombo = myNumber + myOtherNumber = ${myNumber + myOtherNumber}<br><br>`);
+
+let myRemainder = myNumber%5;
+typeof Number;
+
+document.write(`Question 7:<br> let myRemainder = myNumber%5 = ${myNumber%5}<br> typeof Number<br><br>`);
+
+var i = 2;
+typeof Number;
+
+document.write(`Question 8:<br> var i = 2, typeof Number<br><br>`);
+
+var int = 4;
+typeof Number;
+
+document.write(`Question 9:<br> var int = 4, typeof Number<br><br>`);
+
+var myBoolean = new Boolean(false);
+typeof boolean; 
+
+document.write(`Question 10:<br> var myBoolean = new Boolean(false), typeof boolean`);
+
+
+
